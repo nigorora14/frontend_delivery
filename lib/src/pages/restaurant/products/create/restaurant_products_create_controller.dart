82ec6ec5df601ba2db28c2139f66461273c11799
nigorora14 <niltongr@outlook.com>
2021-08-03 09:59:25 -0500

    .

--- lib/src/pages/restaurant/products/create/restaurant_products_create_controller.dart
+++ lib/src/pages/restaurant/products/create/restaurant_products_create_controller.dart
@@ -1,10 +1,17 @@
+
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:frontend_delivery/src/models/category.dart';
+import 'package:frontend_delivery/src/models/product.dart';
 import 'package:frontend_delivery/src/models/response_api.dart';
 import 'package:frontend_delivery/src/models/user.dart';
 import 'package:frontend_delivery/src/provider/categories_provider.dart';
 import 'package:frontend_delivery/src/utils/my_snackbar.dart';
 import 'package:frontend_delivery/src/utils/shared_pref.dart';
+import 'package:image_picker/image_picker.dart';
 
 class RestaurantProductsCreateController{
   BuildContext context;
@@ -12,11 +19,20 @@ class RestaurantProductsCreateController{
 
   TextEditingController nameController= new TextEditingController();
   TextEditingController descriptionController = new TextEditingController();
+  MoneyMaskedTextController priceController= new MoneyMaskedTextController();
 
   CategoriesProvider _categoriesProvider= new CategoriesProvider();
   User user;
-
   SharedPref sharedPref= new SharedPref();
+
+  List<Category> categories = [];
+  String idCategory;//almacena el id de la cetegoria almacenada
+
+  //Imagenes
+  PickedFile pickedFile;
+  File imageFile1;
+  File imageFile2;
+  File imageFile3;
 
   Future init(BuildContext context, Function refresh) async{
     this.context = context;
@@ -24,16 +40,81 @@ class RestaurantProductsCreateController{
 
     user = User.fromJson(await sharedPref.read('user'));
     _categoriesProvider.init(context, user);
+    getCategories();
   }
+  void getCategories() async {
+    categories = await _categoriesProvider.getAll();
+    refresh();
+  }
+
   void createProduct() async{
     String name = nameController.text;
     String description = descriptionController.text;
+    double price = priceController.numberValue;
 
-
-    if(name.isEmpty || description.isEmpty){
+    if(name.isEmpty || description.isEmpty || price == 0){
       MySnackbar.show(context, 'Debe ingresar todos los campos.');
       return;
     }
+    if(imageFile1 == null || imageFile2 == null || imageFile3 == null){
+      MySnackbar.show(context, 'Selecciones 3 imagenes');
+      return;
+    }
+    if(idCategory == null)
+    {
+        MySnackbar.show(context, 'Selecciona la categoria.');
+        return;
+    }
+    Product product = new Product(
+      name: name,
+      description: description,
+      price: price,
+      idCategory: int.parse(idCategory)
+    );
+    print('formulario producto: ${product.toJson()}');
+  }
+  Future selectImage(ImageSource imageSource, int numberFile) async{
+    pickedFile = await ImagePicker().getImage(source: imageSource);
+    if(pickedFile!=null){
 
+      if(numberFile == 1) {
+        imageFile1 = File(pickedFile.path);
+      }
+      else if(numberFile == 2){
+        imageFile2 = File(pickedFile.path);
+      }
+      else if(numberFile == 3){
+        imageFile3 = File(pickedFile.path);
+      }
+    }
+    Navigator.pop(context);
+    refresh();
+  }
+  void showAlertDialog(int numberFile){
+    Widget galleryButton = ElevatedButton(
+        onPressed: (){
+          selectImage(ImageSource.gallery, numberFile);
+        },
+        child: Text('Galeria')
+    );
+    Widget cameraButton = ElevatedButton(
+        onPressed: (){
+          selectImage(ImageSource.camera, numberFile);
+        },
+        child: Text('Camara')
+    );
+    AlertDialog alertDialog=AlertDialog(
+      title: Text('seleccionar imagen'),
+      actions: [
+        galleryButton,
+        cameraButton
+      ],
+    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context){
+          return alertDialog;
+        }
+    );
   }
 }
--- lib/src/pages/restaurant/products/create/restaurant_products_create_page.dart
+++ lib/src/pages/restaurant/products/create/restaurant_products_create_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:frontend_delivery/src/models/category.dart';
 import 'package:frontend_delivery/src/pages/restaurant/products/create/restaurant_products_create_controller.dart';
 import 'package:frontend_delivery/src/utils/my_colors.dart';
 
@@ -29,12 +32,25 @@ class _RestaurantProductsCreatePageState extends State<RestaurantProductsCreateP
       appBar: AppBar(
         title: Text('Nuevo producto'),
       ),
-      body: Column(
+      body: ListView(
         children: [
           SizedBox(height: 30),
           _textFieldName(),
           _textFieldDescripcion(),
-          _textFieldPrice()
+          _textFieldPrice(),
+          Container(
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _cardImage (_con.imageFile1, 1),
+                _cardImage (_con.imageFile2, 2),
+                _cardImage (_con.imageFile3, 3)
+              ],
+            ),
+          ),
+          _dropDownCategories(_con.categories),
         ],
       ),
       bottomNavigationBar: _buttonCreate(),
@@ -44,7 +60,7 @@ class _RestaurantProductsCreatePageState extends State<RestaurantProductsCreateP
   Widget _textFieldName(){
     return Container(
       padding: EdgeInsets.all(10),
-      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
       decoration: BoxDecoration(
           color: MyColors.primaryOpacityColor,
           borderRadius: BorderRadius.circular(15)
@@ -68,9 +84,19 @@ class _RestaurantProductsCreatePageState extends State<RestaurantProductsCreateP
       ),
     );
   }
+  List<DropdownMenuItem<String>> _dropDownItems(List<Category> categories){
+    List<DropdownMenuItem<String>> list = [];
+    categories.forEach((category) { 
+      list.add(DropdownMenuItem(
+        child: Text(category.name),
+        value:  category.id,
+      ));
+    });
+    return list;
+  }
   Widget _textFieldDescripcion(){
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
       padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
           color: MyColors.primaryOpacityColor,
@@ -98,13 +124,13 @@ class _RestaurantProductsCreatePageState extends State<RestaurantProductsCreateP
   Widget _textFieldPrice(){
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10),
-      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
       decoration: BoxDecoration(
           color: MyColors.primaryOpacityColor,
           borderRadius: BorderRadius.circular(15)
       ),
       child: TextField(
-        controller: _con.nameController,
+        controller: _con.priceController,
         maxLines: 1,
         keyboardType: TextInputType.number,
         decoration: InputDecoration(
@@ -122,13 +148,104 @@ class _RestaurantProductsCreatePageState extends State<RestaurantProductsCreateP
       ),
     );
   }
+  Widget _cardImage (File imageFile, int numberFile){
+    return GestureDetector(
+      onTap: (){
+        _con.showAlertDialog(numberFile);
+      },
+      child: imageFile != null ?
+      Card(
+        elevation: 3.0,
+        child: Container(
+          height: 100,
+          width: MediaQuery.of(context).size.width*0.26,
+          child: Image.file(
+              imageFile,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ):
+      Card(
+        elevation: 3.0,
+        child: Container(
+          height: 140,
+          width: MediaQuery.of(context).size.width*0.26,
+          child: Image(
+            image: AssetImage('assets/img/add_image.png')
+          ),
+        ),
+      ),
+    )
+;
+  }
+  Widget _dropDownCategories(List<Category> categories){
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 33),
+    child: Material(
+      elevation: 2.0,
+      color: Colors.white,
+      borderRadius: BorderRadius.all(Radius.circular(5)),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.search,
+                  color: MyColors.primaryColor,
+                ),
+                SizedBox(width: 15),
+                Text(
+                  'Categorias',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16
+                  )
+                )
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: DropdownButton(
+                underline: Container(
+                  alignment: Alignment.centerRight,
+                  child: Icon(
+                    Icons.arrow_drop_down_circle,
+                    color: MyColors.primaryColor,
+                  ),
+                ),
+                elevation: 3,
+                isExpanded: true,
+                hint: Text(
+                  'Selecciona categoria',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16
+                  )
+                ),
+                items: _dropDownItems(categories),
+                value: _con.idCategory,
+                onChanged: (option){
+                  setState(() {
+                    _con.idCategory = option; // estableciendo el valor seleccionado
+                  });
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    ),
+  );
+  }
   Widget _buttonCreate(){
     return Container(
       height: 50,
       width: double.infinity,
       margin: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
       child: ElevatedButton(
-        onPressed: (){},//<----------------------------------------------------
+        onPressed: _con.createProduct,//<----------------------------------------------------
         child: Text('Crear producto'),
         style: ElevatedButton.styleFrom(
             primary: MyColors.primaryColor,
