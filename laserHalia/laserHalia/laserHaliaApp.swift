d5948e6d8b4bf59371866f39df16f70a6fc5e951
nigorora14 <niltongr@outlook.com>
2022-05-27 18:44:34 -0500

    .

---
+++ laserHalia/laserHalia/laserHaliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  laserHaliaApp.swift
+//  laserHalia
+//
+//  Created by Nilton Gonzano Rojas on 27/05/22.
+//
+
+import SwiftUI
+
+@main
+struct laserHaliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ laserHalia/laserHaliaUITests/laserHaliaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  laserHaliaUITestsLaunchTests.swift
+//  laserHaliaUITests
+//
+//  Created by Nilton Gonzano Rojas on 27/05/22.
+//
+
+import XCTest
+
+class laserHaliaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
